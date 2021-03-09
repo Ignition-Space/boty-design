63c0f1305d3654f2846c38ef29c7003b6bc827c3
serializedowen <wjh199455@gmail.com>
2021-03-09 17:23:38 +0800

    Revert "fix(tsconfig): added dedicated tsconfig.json for submodules"
    This reverts commit cbffa914f419f4fa7fc426f5c07c526faf9db007.

--- packages/form-generator/src/hooks/useFormItem.ts
+++ packages/form-generator/src/hooks/useFormItem.ts
@@ -2,7 +2,5 @@ import { useState } from 'react';
 import { FormItemType } from '../internals/symbols';
 
 export default function useFormItem(proto: any) {
-  console.log(Reflect.getMetadata('123', proto, 'hello'));
-
-  console.log(Reflect.getMetadata(FormItemType, proto));
+  Reflect.getMetadata(FormItemType, proto);
 }
