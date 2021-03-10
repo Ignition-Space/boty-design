b6fbab475fa00eec5170722864c92205611c9851
Cookie <yi.you@crgecent.com>
2021-03-10 14:37:29 +0800

    feat: 更新组件构建配置

---
+++ packages/components/src/config/provider.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Cookie
+ * @Date: 2021-03-03 12:00:19
+ * @LastEditors: Cookie
+ * @LastEditTime: 2021-03-03 12:02:03
+ * @Description:
+ */
+export const getPrefixCls = (prefixCls) => {
+    return prefixCls ? `boty-${prefixCls}` : 'boty';
+};
