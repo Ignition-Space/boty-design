8fd39c167d52fceb0748409f4a1bc5f57060799b
serializedowen <wjh199455@gmail.com>
2021-03-17 16:48:06 +0800

    fix(template): bugfix in component generator

--- packages/utils/src/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Cookie
- * @Date: 2021-02-27 16:57:17
- * @LastEditors: Cookie
- * @LastEditTime: 2021-02-27 17:04:30
- * @Description:
- */
-export const tuple = (...args) => args;
-export const tupleNum = (...args) => args;
